829004a93b7287322103accfce168f36406e39ad
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-11-30 00:08:58 +0300

    v1 task

---
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -0,0 +1,43 @@
+package com.example.calculator.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public String printGreeting() {
+        return  "Добро пожаловать в калькулятор";
+    }
+
+    public String plus(Integer x, Integer y) {
+        checkData(x, y);
+        Integer z = x + y;
+        return String.valueOf(x) + " + " + String.valueOf(y) + " = " + String.valueOf(z);
+    }
+    public String minus(int x, int y) {
+        checkData(x, y);
+        Integer z = x - y;
+        return String.valueOf(x) + " - " + String.valueOf(y) + " = " + String.valueOf(z);
+    }
+    public String multiply(Integer x, Integer y) {
+        checkData(x, y);
+        Integer z = x * y;
+        return String.valueOf(x) + " * " + String.valueOf(y) + " = " + String.valueOf(z);
+    }
+
+    public String divide(Integer x, Integer y) {
+        checkData(x, y);
+        if (y == 0) {
+            throw new IllegalArgumentException("Деление на ноль");
+        }
+        Double z = (double) x / y;
+        return String.valueOf(x) + " / " + String.valueOf(y) + " = " + String.valueOf(z);
+    }
+
+    public void checkData(Integer x, Integer y) {
+        if (x == null || y == null) {
+            throw new IllegalArgumentException("Не заполнено число");
+        }
+    }
+
+
+
+}
